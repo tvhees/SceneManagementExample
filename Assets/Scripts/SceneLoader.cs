d0dda9226f56b7fa9abeeaa34d39e2d33f6783b9
tvhees <t.vanheeswijck@gmail.com>
2016-02-01 18:46:28 +1030

    First commit
    Basic scene loading and unloading demonstrated by script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (LoadScene ("Scene1", 2.0f));
+		StartCoroutine (LoadScene ("Scene2", 4.0f));
+
+		StartCoroutine (UnloadScene ("Scene1", 6.0f));
+		StartCoroutine (UnloadScene ("Scene2", 8.0f));
+	}
+
+	IEnumerator LoadScene(string sceneName, float delay){
+
+		yield return new WaitForSeconds (delay);
+
+		SceneManager.LoadScene (sceneName, LoadSceneMode.Additive);
+	}
+
+	IEnumerator UnloadScene(string sceneName, float delay){
+		yield return new WaitForSeconds (delay);
+
+		if(SceneManager.GetSceneByName(sceneName)!=null)
+			SceneManager.UnloadScene (sceneName);
+	}
+}
